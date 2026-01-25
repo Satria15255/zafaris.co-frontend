a279ab61d46b0971eef5d11011508f788a259a10
satriazafarani <satriazafarani05@gmail.com>
2026-01-25 15:34:51 +0700

    update hero section

--- src/page/Banner.js
+++ src/page/Banner.js
@@ -10,7 +10,7 @@ function Banner() {
         <div className=" flex w-full h-[80vh] md:h-[65vh] lg:h-[85vh] z-0 rounded-3xl md:rounded-5 lg:rounded-3xl bg-center bg-cover" style={{ backgroundImage: `url(${Slide2})` }}>
           <div className="grid grid-cols-1 md:grid-cols-2 items-center w-100 h-full rounded-3xl ">
             <div className="p-3 flex flex-col gap-1 lg:space-y-5">
-              <h2 className="text-left text-3xl md:text-4xl lg:text-6xl drop-shadow-2xl font-bold flex flex-col">
+              <h2 className="text-left text-4xl md:text-4xl lg:text-6xl drop-shadow-2xl font-bold flex flex-col">
                 Walk in Comfort <span>Step in Style.</span>
               </h2>
               <p className="text-left text-xs md:text-xs lg:text-sm max-w-sm text-gray-500 drop-shadow-2xl md:mt-2 ">
@@ -20,9 +20,9 @@ function Banner() {
                 <Nav.Link
                   as={NavLink}
                   to="/products"
-                  className="flex items-center justify-center text-sm md:text-sm lg:text-lg w-1/2 md:w-2/5 py-1 md:py-6 md:h-[8vh] lg:h-[8vh] border border-black rounded-full  hover:bg-white hover:text-black text-center bg-gray-900 text-gray-100 transition duration-100"
+                  className="flex items-center justify-center text-sm md:text-sm lg:text-lg w-1/2 md:w-2/5 py-1 md:py-6 md:h-[8vh] lg:h-[8vh] border border-black rounded-full  hover:bg-black hover:text-white text-center font-semibold text-gray-900 transition duration-100"
                 >
-                  Discover
+                  DISCOVER
                 </Nav.Link>
               </div>
             </div>
